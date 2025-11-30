ae21cd55a06065eb7437910fb8afd2bc44508db5
filasouf2005 <nefsiabdelouahab@gmail.com>
2025-12-01 00:36:29 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,39 @@
+# a = int(input("Enter a number: "))
+# print('This is a :', a)
+# name = str(input('Enter your name : '))
+# print ('the name is  : ',name)
+
+# print(round(3.5305,0))
+# print(divmod(27,5))
+# print(isinstance(1,(str,float)))
+# print(pow(2,2,2))
+
+
+# help(pow)
+
+# a = int(input ())
+# b = int(input())
+
+# if a>=b : 
+#     print(a)
+# else:
+#     print(b)
+"""
+Line 
+Comment
+"""
+# i=1
+# while i < 10 : 
+#     print(i)
+#     i += 1
+# L= []
+# for i in range(10):
+#     print(i)
+#     L.append(i**2)
+# print(L)
+
+# L = [2, 3, 1, -8]
+# print(sorted(L))
+import math
+
+print(math.sqrt(25)) 
